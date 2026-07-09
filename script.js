cb7aadffad2f41b94caa04aaf78b1f6493ed306c
monipe26 <info.girlsloveplay@gmail.com>
2026-07-09 18:46:42 -0300

    p

--- script.js
+++ script.js
@@ -1067,19 +1067,8 @@ function onYouTubeIframeAPIReady() {
           if (idx >= 0) lastPlaylistIndex = idx;
           actualizarEpisodio();
           actualizarTituloYoutube();
-          if (player.setOption && player.getOption) {
-            const tracks = player.getOption("captions", "tracklist") || [];
-            const trackEs = tracks.find((t) => t.languageCode === "es");
-            const trackAuto = tracks.find((t) => t.kind === "asr");
-
-            if (trackEs) {
-              player.setOption("captions", "track", { languageCode: "es" });
-            } else if (trackAuto) {
-              player.setOption("captions", "track", {
-                languageCode: trackAuto.languageCode,
-              });
-            }
-          }
+          if (player.setOption)
+            player.setOption("captions", "track", { languageCode: "es" });
 
           // 💾 arranca a guardar el progreso cada 5s mientras se reproduce
           if (!progressInterval) {
