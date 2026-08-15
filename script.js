1dc60127fed589bab80ea34cab76f19b5b62ca58
monipe26 <info.girlsloveplay@gmail.com>
2026-08-15 18:51:50 -0300

    5

--- script.js
+++ script.js
@@ -97,6 +97,8 @@ const comunidadArray = [
 // ✅ Solo necesitás pegar el videoId — título y miniatura se cargan solos
 // =====================
 const ostsArray = [
+  
+  { id: "ost112", videoId: "hpbb1xLc-tE" },
   { id: "ost112", videoId: "Ra0ZOFiHoEI" },
   { id: "ost111", videoId: "jad-V_nyvaY" },
   { id: "ost110", videoId: "V0b-54I-1-0" },
