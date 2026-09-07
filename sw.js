5d5eff5e16ffd16caa22e58672ce0f6f967a08fb
monipe26 <info.girlsloveplay@gmail.com>
2026-09-07 13:24:22 -0300

    1

--- sw.js
+++ sw.js
@@ -1,18 +1,23 @@
-const CACHE_NAME = 'glplay-v1';
+const CACHE_NAME = "glplay-v1";
 
-self.addEventListener('install', (event) => {
+self.addEventListener("install", (event) => {
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (event) => {
+self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null)))
-    ).then(() => self.clients.claim())
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(
+          keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null))
+        )
+      )
+      .then(() => self.clients.claim())
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
     fetch(event.request).catch(() => caches.match(event.request))
   );
